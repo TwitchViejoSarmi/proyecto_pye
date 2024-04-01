093f2c9fa30cfecba45c0cea9e8fb21d0e644122
TwitchViejoSarmi <alesansarive@gmail.com>
2024-04-01 16:05:01 -0500

    NEW: Avance inicial del proyecto.

---
+++ proyecto_pye/Codigo.R
@@ -0,0 +1,14 @@
+library(readxl)
+Base_Datos <- read.csv("Base de Datos/Datos.csv")
+
+# Variables punto 1.
+print(Base_Datos$PESO..Gramos.)
+print(Base_Datos$AREA.RESIDENCIA)
+
+# Variables punto 2.
+print(Base_Datos$SITIO.NACIMIENTO)
+print(Base_Datos$AREA.RESIDENCIA)
+
+#Variables punto 3.
+print(Base_Datos$PESO..Gramos.)
+print(Base_Datos$TALLA..Centímetros.)
